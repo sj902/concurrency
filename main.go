762e21dc5d1ce82ff80b54eb24aeab505e504e77
sj902 <shubhamj@tuta.io>
2023-08-08 23:08:51 +0530

    ch3 once

--- main.go
+++ main.go
@@ -7,36 +7,11 @@ import (
 
 func main() {
 
-	type Button struct {
-		Clicked *sync.Cond
-	}
-	button := Button{Clicked: sync.NewCond(&sync.Mutex{})}
-	subscribe := func(c *sync.Cond, fn func()) {
-		var goroutineRunning sync.WaitGroup
-		goroutineRunning.Add(1)
-		go func() {
-			goroutineRunning.Done()
-			c.L.Lock()
-			defer c.L.Unlock()
-			c.Wait()
-			fn()
-		}()
-		goroutineRunning.Wait()
-	}
-	var clickRegistered sync.WaitGroup
-	clickRegistered.Add(3)
-	subscribe(button.Clicked, func() {
-		fmt.Println("Maximizing window.")
-		clickRegistered.Done()
-	})
-	subscribe(button.Clicked, func() {
-		fmt.Println("Displaying annoying dialog box!")
-		clickRegistered.Done()
-	})
-	subscribe(button.Clicked, func() {
-		fmt.Println("Mouse clicked.")
-		clickRegistered.Done()
-	})
-	button.Clicked.Broadcast()
-	clickRegistered.Wait()
+	var count int
+	increment := func() { count++ }
+	decrement := func() { count-- }
+	var once sync.Once
+	once.Do(increment)
+	once.Do(decrement)
+	fmt.Printf("Count: %d\n", count)
 }
